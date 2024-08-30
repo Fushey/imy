998611bbd85b5e1ddf12e8152d7e8496536d08e4
Fushey <jansen.tobias@gmail.com>
2024-08-30 14:30:37 +0800

    Update login.php

--- pages/login.php
+++ pages/login.php
@@ -64,7 +64,7 @@ document.getElementById('login-form').addEventListener('submit', function(e) {
     statusMessage.classList.remove('hidden', 'bg-red-100', 'text-red-700', 'bg-green-100', 'text-green-700');
     statusMessage.classList.add('bg-blue-100', 'text-blue-700');
 
-    fetch('http://localhost:5000/login', {
+    fetch('https://api.immoyes.com/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
