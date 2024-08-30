74c1511dc8bf7bf626137c33b040a40d9faaf80c
Fushey <jansen.tobias@gmail.com>
2024-08-30 14:32:37 +0800

    Update set_session.php

--- set_session.php
+++ set_session.php
@@ -3,9 +3,9 @@ session_start();
 
 $data = json_decode(file_get_contents('php://input'), true);
 
-if (isset($data['user_id']) && isset($data['username'])) {
+if (isset($data['user_id']) && isset($data['email'])) {
     $_SESSION['user_id'] = $data['user_id'];
-    $_SESSION['username'] = $data['username'];
+    $_SESSION['email'] = $data['email'];
     echo json_encode(['success' => true]);
 } else {
     echo json_encode(['success' => false, 'message' => 'Invalid data']);
